7e8deb373caddaf5269defc0916c5be484aa49b8
Estwaldstein <andrea.toscani@mac.com>
2025-05-10 17:07:50 +0200

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://app.exchange.newlink-asia.com/api',
+  baseURL: 'https://api.exchange.newlink-asia.com/api',
   headers: {
     'Content-Type': 'application/json'
   },
