e945d14c6811fea735e8683bdae855b604adf71c
Estwaldstein <andrea.toscani@mac.com>
2025-09-22 18:49:28 +0200

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -50,7 +50,7 @@ const HomePage = () => {
             </ul>
           </nav>
 
-          <div className="contact-email">📧 info@newlink-asia.com</div>
+          <div className="contact-email">📧 info@swiss-starter.ch</div>
         </div>
       </header>
 
